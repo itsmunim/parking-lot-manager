46d27043a4f952a60b10c2fb6ac9fe2239919ed3
dibosh <md-abdul_munim@astro.com.my>
2019-05-26 02:43:26 +0800

    Add support for file interface
       Output is ok; but not same as pdf; will look into it
       tomorrow

--- program/src/index.ts
+++ program/src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-import {createInterface} from 'readline';
+import {createReadStream, existsSync} from 'fs';
+import {createInterface, ReadLineOptions} from 'readline';
 import {CommandExecutor, InterfaceType} from './core/services/command.executor';
 
 function _isInteractive(): boolean {
@@ -8,17 +9,51 @@ function _isInteractive(): boolean {
   return process.argv.length === 2;
 }
 
-function run(): void {
-  let commandExecutor = null;
-  let interfaceConfig = {
+function _getInterfaceConfig(filePath?: string): ReadLineOptions {
+  if (filePath) {
+    if (!existsSync(filePath)) {
+      throw new Error('Must be a valid file path');
+    }
+
+    return {
+      input: createReadStream(filePath),
+      output: process.stdout
+    }
+  }
+
+  /**
+   * It might look like this could be achieved like below-
+   * let iConfig = {output: process.stdout};
+   * ...
+   * if (filePath) {
+   *   ...
+   *   iConfig = createReadStream(filePath);
+   *} else {
+   *   iConfig = process.stdin;
+   *}
+   *
+   * return iConfig;
+   *
+   * But that approach raises a TS error -
+   * TS2739: Type 'ReadStream' is missing the following properties from type 'ReadStream': writable, write, end
+   *
+   * So this minor duplication is necessary.
+   */
+  return {
     input: process.stdin,
     output: process.stdout
   };
+}
 
-  if (_isInteractive()) {
-    commandExecutor = new CommandExecutor(createInterface(interfaceConfig), InterfaceType.STDIN);
-    commandExecutor.process();
-  }
+function run(): void {
+  const interfaceType = _isInteractive() ? InterfaceType.STDIN : InterfaceType.File;
+  const interfaceConfig = _getInterfaceConfig(
+    interfaceType === InterfaceType.File
+      ? process.argv[2] : null
+  );
+
+  const commandExecutor = new CommandExecutor(createInterface(interfaceConfig), interfaceType);
+  commandExecutor.process();
 }
 
 run();
